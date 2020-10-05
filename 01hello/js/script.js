c6345ad401885dd4ba94123946999f1aa1a118f5
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-05 12:55:23 +0200

    structure conditionnelle

--- 01hello/js/script.js
+++ 01hello/js/script.js
@@ -61,3 +61,57 @@ console.log(somme); // 7
 //  - avec "let" : idem que var, mais portée de bloc
 //  - avec "const" : idem que let, mais ne peut être réaffecté
 // utilisation en générale 90% const, 10% let, les autres quasi jamais
+
+/********************************
+ * structure de contrôle
+ * 
+ * Principe: contrôler l'exécution (ou non) de certaines instructions
+ ********************************/
+
+/********************************
+ * Structure conditionnelle
+ ********************************/
+if (12 > somme) { // évalue une expression booléenne
+    // si VRAI : exécution du bloc suivant
+    console.log('La somme est inférieure à 12');
+}
+
+if (12 > somme) { // évalue une expression booléenne
+    // si VRAI : exécution du bloc suivant
+    console.log('La somme est inférieure à 12');
+}
+else {
+    // possibilité d'avoir un "sinon"
+    console.log('La somme est supérieure ou égale à 12');
+    if (25 > somme) {
+
+    }
+}
+
+if (12 > somme) { // évalue une expression booléenne
+    // si VRAI : exécution du bloc suivant
+    console.log('La somme est inférieure à 12');
+}
+else if (25 > somme) {
+     // si juste le if à l'intérieur du else
+}
+
+// peu utilisé car:
+//  - ne fonctionne qu'avec une correspondance exacte
+//  - uniquement sur des données primitives
+//  - nécessite de changer le code source si une valeur supplémentaire à gérer
+switch(somme) {
+    case 25: // case = condition pour démarrer l'exécuion des instructions
+        console.log('la somme vaut 25');
+        break; // on "force" la sortie du bloc avec "break"
+    case 12:
+    case 32:
+        console.log('la somme vaut 12 ou 32');
+    case 11:
+        console.log('la somme vaut 12 ou 32 ou 11');
+        break;
+    default:
+        console.log('la somme vaut tout 25, 12, 32 et 11');
+}
+
+

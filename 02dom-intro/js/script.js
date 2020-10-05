4e72c122b67a131f80bcfd381a0fdd557df10809
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-05 14:45:17 +0200

    événement

--- 02dom-intro/js/script.js
+++ 02dom-intro/js/script.js
@@ -2,8 +2,9 @@
 
 // DOM
 // Document Object Model
+// variable document mise à disposition auto par la navigateur
 
-// aller chercher un élément du DOM
+// aller chercher un élément du DOM par rapport à son ID
 const article = document.getElementById('article-1234');
 // modifier le DOM
 // accès aux propriétés via "."
@@ -17,3 +18,10 @@ for (const paragraphe of paragraphes) {
     paragraphe.style.color = 'green';
 }
 
+const liens = document.querySelectorAll('.menu a');
+
+for (const lien of liens) {
+    lien.addEventListener('click', function() {
+        console.log('click');
+    });
+}

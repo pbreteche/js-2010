e8f6c84b988ffde3e70b20e8555c30a22bcfe063
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-06 11:50:55 +0200

    creation données

--- 07agenda/js/script.js
+++ 07agenda/js/script.js
@@ -5,12 +5,24 @@
  */
 const Agenda = function(rootElement) {
     this.root = rootElement;
+    this.concerts = [];
 }
 
 Agenda.prototype.init = function() {
+    this.concerts.push(new Concert('Concert 1', 'Rock'));
+    this.concerts.push(new Concert('Concert 2', 'Jazz'));
+    this.concerts.push(new Concert('Concert 3', 'Rap'));
+    this.concerts.push(new Concert('Concert 4', 'Rap'));
+    this.concerts.push(new Concert('Concert 5', 'Rock'));
     this.root.innerHTML = '<h1>Agenda</h1>';
+}
+
+const Concert = function(title, style) {
+    this.title = title;
+    this.style = style;
 }
 
 const myAgenda = new Agenda(document.querySelector('#agenda'));
 console.log(myAgenda);
 myAgenda.init();
+console.log(myAgenda);

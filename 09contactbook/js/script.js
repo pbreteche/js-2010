1c83e021c997154e72350e305318fdd8369f9253
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-07 17:07:00 +0200

    fin recherche carnet adresse

--- 09contactbook/js/script.js
+++ 09contactbook/js/script.js
@@ -30,11 +30,42 @@ ContactBook.prototype.init = function() {
         for (const contactFound of found) {
             const li = document.createElement('li');
             li.textContent = contactFound.name;
+            li.setAttribute('tabindex', '-1')
             list1.appendChild(li);
         }
         this.after(list1);
-
     })
+
+    this.root.addEventListener('keyup', function(event){
+        const list = this.querySelector('ul');
+        if (!list) {
+            return;
+        }
+
+        const focusElt = list.querySelector(':focus');
+            
+        if (event.code === 'ArrowDown') {
+            if(!focusElt) {
+                list.children[0].focus();
+            }
+            else if (focusElt.nextElementSibling) {
+                    focusElt.nextElementSibling.focus();
+            }
+        }
+        if (event.code === 'ArrowUp') {
+            if(!focusElt) {
+                list.children[list.children.length-1].focus();
+            }
+            else if (focusElt.previousElementSibling) {
+                    focusElt.previousElementSibling.focus();
+            }
+        }
+        if (event.code === 'Enter') {
+            if(focusElt) {
+                list.nextElementSibling.textContent += focusElt.textContent;
+            }
+        }
+    });
 }
 
 ContactBook.prototype.display = function() {

f7d141f7a8d48d82dc7d4672d6bfb9b50a5e2104
Pierre Bretéché <pbreteche@dawan.fr>
2020-10-07 11:25:52 +0200

    remonter image précédente

--- 08gallery/js/script.js
+++ 08gallery/js/script.js
@@ -8,7 +8,11 @@ const Gallery = function(selector) {
 Gallery.prototype.init = function() {
     this.images = IMAGE_LIST;
     this.root.innerHTML = `
-<div id="gallery-viewer"><img src="img/${this.images[0]}"></div>
+<div id="gallery-viewer"><img src="img/${this.images[0]}">
+<div class="sort-action">
+<button>&#9664;</button><button>&#9654;</button>
+</div>
+</div>
 <nav id="gallery-nav">
     <ul>
     ${this.images.reduce(function(accumulator, image) {
@@ -24,7 +28,34 @@ Gallery.prototype.init = function() {
     for (const link of navLinks) {
         link.addEventListener('click', function(){
             viewerImage.src = this.children[0].src;
-        })
+        });
+    }
+
+    const sortButtons = this.root.querySelectorAll('.sort-action button');
+
+    const that = this;
+
+    for (const button of sortButtons) {
+        button.addEventListener('click', function(){
+            // à partir de l'URL complète de l'image
+            // on découpe selon les "/"
+            // et on ne conserve que la dernière partie
+            const imageFilename = decodeURI(viewerImage.src.split('/').pop());
+
+            // Autre manière de faire la même chose:
+            // à partir de l'URL complète de l'image
+            // on cherche le premier "/" à partir de la fin "lastIndexOf"
+            // et on coupe après la position de ce caractère "substring"
+            const pos = viewerImage.src.lastIndexOf('/') + 1;
+            const imageFilename2 = decodeURI(viewerImage.src.substring(pos));
+
+            const currentImageIndex = that.images.indexOf(imageFilename);
+
+            console.log(currentImageIndex, imageFilename);
+
+            viewerImage.src = 'img/' + that.images[currentImageIndex-1];
+            
+        });
     }
 }
 
